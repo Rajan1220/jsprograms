6bba236a6336bb8f3cbfc274c2314368e6e2a10c
rajan1220 <rajan.gcd@gmail.com>
2015-09-15 12:04:24 +0530

    Program to find a missing element from array

---
+++ missingelement.js
@@ -0,0 +1,19 @@
+, j;
+
+for (i =0; i < max; i++) 
+{
+	ref[i] = [i];
+}
+for (i = 0; i < arr.length; i++) 
+{
+	for (j = 0; j < ref.length; j++) 
+	{
+		if (arr[i] == ref[j]) 
+		{
+			ref.splice(j, 1);
+		 	break;
+		}
+	}
+}
+
+alert('missing numbers are ' + ref;

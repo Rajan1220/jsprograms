f28f6ad57a8cfa5548e94439b6746ad259fd868d
rajan1220 <rajender.r@gai.co.in>
2016-10-24 22:45:08 +0530

    Modified html js and php files as variables passed through js file are not accpted into php file due to wrong variable passed to php. It should use variables passed through js.

--- signuptologin/process.php
+++ signuptologin/process.php
@@ -5,28 +5,17 @@
 	$database = "js_db_2016";
 
 	$signupuser = $_POST['username'];
-	$email = $_POST['email'];
-	$pass = $_POST['pass'];
-
-	//	echo $username . $email . $pass;
-	// Create connec t ion
+	$email = $_POST['uemail'];
+	$pass = $_POST['upass'];
   $conn = new mysqli($servername, $username, $password, $database);
-  // Check connect n 
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  //echo "Connected successfully";
   $sql = "INSERT INTO usersignup (username, email, password) VALUES ('$signupuser', '$email', '$pass')";
-
   if ($conn->query($sql) === TRUE) {
     echo "You Are successfully Sign Up !";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
-
-$conn->close();
-	
-
-	
-
+  $conn->close();
 ?>
